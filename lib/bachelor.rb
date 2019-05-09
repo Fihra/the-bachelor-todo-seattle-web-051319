58f318e19cb5c2335a976519b74261cd9b0d5dcb
Fabian Fabro <fabrofabian@gmail.com>
2019-05-09 02:59:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -61,7 +61,7 @@ def get_average_age_for_season(data, season)
   data.each do |seasonNum, contestant|
     if seasonNum == season
       contestant.each do |k|
-        age_array << k["age"]
+        age_array << k["age"].to_f
       end
     end
   end
