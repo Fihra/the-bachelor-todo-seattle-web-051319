056a8d3c5442122ab03697ea2d54112c2c36fc59
Fabian Fabro <fabrofabian@gmail.com>
2019-05-09 03:02:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -70,7 +70,6 @@ def get_average_age_for_season(data, season)
     total += i
   }
   total = total/age_array.length
-  total.to_f.ceil
-  return total
+  return total.to_f.ceil
   #binding.pry
 end
